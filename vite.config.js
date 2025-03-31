68e0d13d234d67f24846617f14b77c489a323962
Tim Heuer <tim@timheuer.com>
2025-03-31 13:13:07 -0700

    fixing favicon

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,29 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: './'
+  plugins: [
+    react(),
+    {
+      name: 'html-transform',
+      transformIndexHtml(html) {
+        return html.replace(
+          '</head>',
+          '  <link rel="icon" type="image/x-icon" href="./favicon.ico" />\n  </head>'
+        )
+      }
+    }
+  ],
+  base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === 'favicon.ico') {
+            return 'favicon.ico';
+          }
+          return 'assets/[name]-[hash][extname]';
+        }
+      }
+    }
+  }
 })
